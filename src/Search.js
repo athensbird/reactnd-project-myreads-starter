143579f0f0f7871312b7878d8dcd1cf822202e00
athensbird <popularwatson@gmail.com>
2017-11-18 14:55:28 -0600

    style fix

--- src/Search.js
+++ src/Search.js
@@ -33,30 +33,30 @@ class Search extends React.Component {
               onChange={(event) => this.updateQuery(event.target.value)}
             />
           </div>
-          <div className="search-books-results">
-            <ol className="books-grid">
-              {books && books.map((book) => (
-                <li key={book.id}>
-                  <div className="book">
-                    <div className="book-top">
-                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks ? book.imageLinks.thumbnail : null})`}}></div>
-                      <div className="book-shelf-changer">
-                        <select value={book.shelf ? book.shelf : "none"} onChange={(e) => this.props.handleShelf(book, e.target.value)}>
-                          <option value="none" disabled>Move to...</option>
-                          <option value="currentlyReading">Currently Reading</option>
-                          <option value="wantToRead">Want to Read</option>
-                          <option value="read">Read</option>
-                          <option value="none">None</option>
-                        </select>
-                      </div>
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid">
+            {books && books.map((book) => (
+              <li key={book.id}>
+                <div className="book">
+                  <div className="book-top">
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks ? book.imageLinks.thumbnail : null})`}}></div>
+                    <div className="book-shelf-changer">
+                      <select value={book.shelf ? book.shelf : "none"} onChange={(e) => this.props.handleShelf(book, e.target.value)}>
+                        <option value="none" disabled>Move to...</option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                        <option value="none">None</option>
+                      </select>
                     </div>
-                    <div className="book-title">{book.title}</div>
-                    <div className="book-authors">{book.authors ? book.authors[0] : book.authors}</div>
                   </div>
-                </li>
-              ))}
-            </ol>
-          </div>
+                  <div className="book-title">{book.title}</div>
+                  <div className="book-authors">{book.authors ? book.authors[0] : book.authors}</div>
+                </div>
+              </li>
+            ))}
+          </ol>
         </div>
       </div>
     );
